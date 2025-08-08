a44fafa05262e30db0329cb5f9cd12fd8b70c700
linhdh52 <liq.97tn@gmail.com>
2025-08-08 15:07:49 +0700

    router

---
+++ src/app/app-rounting.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {LoginComponent} from "./login/login.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
